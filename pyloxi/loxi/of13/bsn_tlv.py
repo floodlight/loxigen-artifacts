2fd01623ffcb3bff440200abc528fd2b3020e917
abat <abat@bigswitch.com>
2014-01-28 19:01:01 -0800

    Artifacts from https://github.com/floodlight/loxigen/compare/f8498dd3102a8e03596d821c1b1c9ca80e4145d7...074f7096e8fd4e7a63d2b9cbba68e60be9a87f31
    Loxigen Head commit floodlight/loxigen@074f7096e8fd4e7a63d2b9cbba68e60be9a87f31
    commit 074f7096e8fd4e7a63d2b9cbba68e60be9a87f31
    Merge: f8498dd 038c9e6
    Author: abat <abat@bigswitch.com>
    Date:   Tue Jan 28 18:58:32 2014 -0800
    
        Merge into master from pull request #186:
        pyloxi: parse unknown subclasses as the superclass (https://github.com/floodlight/loxigen/pull/186)
    
    commit 038c9e6106e8756c1cd152bbbdc2ee75ffba7689
    Author: Rich Lane <rlane@bigswitch.com>
    Date:   Mon Jan 27 15:33:18 2014 -0800
    
        pyloxi: parse unknown subclasses as the superclass
    
        Previously we would throw an exception if we encountered a (for example)
        experimenter action subclass that we didn't know existed. This was fine for
        OFTest but causes problems for other tools that use pyloxi.
    
        This change generates a full set of pack/unpack/pretty_print functions for
        virtual classes. If a discriminator field contains a subtype we don't know
        about, the virtual class is instantiated instead.
    
        This also removes the special cases around unknown hello elements.

--- pyloxi/loxi/of13/bsn_tlv.py
+++ pyloxi/loxi/of13/bsn_tlv.py
@@ -23,14 +23,48 @@ import loxi.generic_util
 class bsn_tlv(loxi.OFObject):
     subtypes = {}
 
+
+    def __init__(self, type=None):
+        if type != None:
+            self.type = type
+        else:
+            self.type = 0
+        return
+
+    def pack(self):
+        packed = []
+        packed.append(struct.pack("!H", self.type))
+        packed.append(struct.pack("!H", 0)) # placeholder for length at index 1
+        length = sum([len(x) for x in packed])
+        packed[1] = struct.pack("!H", length)
+        return ''.join(packed)
+
     @staticmethod
     def unpack(reader):
         subtype, = reader.peek('!H', 0)
-        try:
-            subclass = bsn_tlv.subtypes[subtype]
-        except KeyError:
-            raise loxi.ProtocolError("unknown bsn_tlv subtype %#x" % subtype)
-        return subclass.unpack(reader)
+        subclass = bsn_tlv.subtypes.get(subtype)
+        if subclass:
+            return subclass.unpack(reader)
+
+        obj = bsn_tlv()
+        obj.type = reader.read("!H")[0]
+        _length = reader.read("!H")[0]
+        orig_reader = reader
+        reader = orig_reader.slice(_length - (2 + 2))
+        return obj
+
+    def __eq__(self, other):
+        if type(self) != type(other): return False
+        if self.type != other.type: return False
+        return True
+
+    def pretty_print(self, q):
+        q.text("bsn_tlv {")
+        with q.group():
+            with q.indent(2):
+                q.breakable()
+            q.breakable()
+        q.text('}')
 
 
 class broadcast_query_timeout(bsn_tlv):
