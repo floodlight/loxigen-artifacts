d716eb3aefdbc2a1d36387381193f2de97819333
abat <abat@bigswitch.com>
2016-03-28 10:15:32 -0700

    Artifacts from https://github.com/floodlight/loxigen/compare/7743b122bb78e4752430e534a20e8f4090ce8794...3c24219a38dddc25bb15252c4dce0585ebff6d9b
    Loxigen Head commit floodlight/loxigen@3c24219a38dddc25bb15252c4dce0585ebff6d9b
    commit 3c24219a38dddc25bb15252c4dce0585ebff6d9b
    Merge: 7743b12 a5bae56
    Author: abat <abat@bigswitch.com>
    Date:   Mon Mar 28 09:58:26 2016 -0700
    
        Merge into master from pull request #473:
        Make Bundle-Add inherit the XID of the nested message  #loxigen/454 (https://github.com/floodlight/loxigen/pull/473)
    
    commit a5bae5632b453c377e5c0c8072bb5c7bebf54446
    Author: Andreas Wundsam <andreas.wundsam@bigswitch.com>
    Date:   Mon Mar 28 06:04:02 2016 -0700
    
        Make Bundle-Add inherit the XID of the containing message  #loxigen/454
    
        This ensures the generated messages conform to the OF Spec, 1.4,
        section 7.3.9.6 Adding messages to a bundle:
    
        > Message added in a bundle should have a unique xid to help matching
        > errors to messages, and the xid of the bundle add message must be the
        > same.

--- openflowj/gen-src/main/java/org/projectfloodlight/openflow/protocol/ver14/OFBundleAddMsgVer14.java
+++ openflowj/gen-src/main/java/org/projectfloodlight/openflow/protocol/ver14/OFBundleAddMsgVer14.java
@@ -166,12 +166,22 @@ class OFBundleAddMsgVer14 implements OFBundleAddMsg {
         return data;
     }
 
+    /** Custom setter that ensures the BundleAdd message inherits the XID from their
+     *  contained message, as per OF Spec 1.4.0:
+     *  <p>
+     *  7.3.9.6 Adding messages to a bundle
+     *  </p><p>
+     *     Message added in a bundle should have a unique xid to help matching errors to messages,
+     *     and the xid of the bundle add message must be the same.
+     *  </p>
+     */
     @Override
     public OFBundleAddMsg.Builder setData(OFMessage data) {
         this.data = data;
         this.dataSet = true;
-        return this;
+        return setXid(data.getXid());
     }
+
 
 
         @Override
@@ -257,12 +267,22 @@ class OFBundleAddMsgVer14 implements OFBundleAddMsg {
         return data;
     }
 
+    /** Custom setter that ensures the BundleAdd message inherits the XID from their
+     *  contained message, as per OF Spec 1.4.0:
+     *  <p>
+     *  7.3.9.6 Adding messages to a bundle
+     *  </p><p>
+     *     Message added in a bundle should have a unique xid to help matching errors to messages,
+     *     and the xid of the bundle add message must be the same.
+     *  </p>
+     */
     @Override
     public OFBundleAddMsg.Builder setData(OFMessage data) {
         this.data = data;
         this.dataSet = true;
-        return this;
+        return setXid(data.getXid());
     }
+
 //
         @Override
         public OFBundleAddMsg build() {
