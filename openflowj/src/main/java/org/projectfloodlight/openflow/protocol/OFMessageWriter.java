33b9acdeba6af234d3694210ccb8c6671a749adf
Jenkins <noreply@bigswitch.com>
2017-05-18 17:45:38 -0700

    Artifacts from https://github.com/floodlight/loxigen/compare/185524cda8374eef1cc9633c68cfef60a55fe6d6...77517c16bc45d56f623bf9e789585f5b30209c1a (Branch HEAD)
    Loxigen Head commit floodlight/loxigen@77517c16bc45d56f623bf9e789585f5b30209c1a
    commit 77517c16bc45d56f623bf9e789585f5b30209c1a
    Merge: 185524c 25dc665
    Author: Kiran Poola <kiran.poola@bigswitch.com>
    Date:   Fri May 19 00:35:12 2017 +0000
    
        Merge 25dc6658d80658b53a0fd6c8c5349d137b19d424 into 185524cda8374eef1cc9633c68cfef60a55fe6d6
    
    commit 25dc6658d80658b53a0fd6c8c5349d137b19d424
    Author: Kiran Poola <kiran.poola@bigswitch.com>
    Date:   Thu May 18 17:34:50 2017 -0700
    
        SWL-3777 outer_src_mac, virtual_ip tlvs.

--- openflowj/src/main/java/org/projectfloodlight/openflow/protocol/OFMessageWriter.java
+++
@@ -1,8 +0,0 @@
-package org.projectfloodlight.openflow.protocol;
-
-import io.netty.buffer.ByteBuf;
-import org.projectfloodlight.openflow.exceptions.OFParseError;
-
-public interface OFMessageWriter<T> {
-    public void write(ByteBuf bb, T message) throws OFParseError;
-}
