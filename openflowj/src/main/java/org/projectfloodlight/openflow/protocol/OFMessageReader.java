8c9814c33f482b21806fe33e239ac8a99b2cc87e
Jenkins <noreply@bigswitch.com>
2017-09-23 13:49:20 -0700

    Artifacts from https://github.com/floodlight/loxigen/compare/48de0a4852272346b5c1336de9601581b028c3a9...ec8dca42103fb7a267437806c76b0621de07ef50 (Branch HEAD)
    Loxigen Head commit floodlight/loxigen@ec8dca42103fb7a267437806c76b0621de07ef50
    commit ec8dca42103fb7a267437806c76b0621de07ef50
    Merge: 48de0a4 621a8ed
    Author: Prem Shankar Sharma <premshankar.sharma@bigswitch.com>
    Date:   Sat Sep 23 20:46:29 2017 +0000
    
        Merge 621a8ed26d68066374c1215ce79e6084e31e30fb into 48de0a4852272346b5c1336de9601581b028c3a9
    
    commit 621a8ed26d68066374c1215ce79e6084e31e30fb
    Author: Prem Sharma <premshankar.sharma@bigswitch.com>
    Date:   Sat Sep 23 13:43:43 2017 -0700
    
        Reverting the addition of TLV for digest algorithm

--- openflowj/src/main/java/org/projectfloodlight/openflow/protocol/OFMessageReader.java
+++
@@ -1,8 +0,0 @@
-package org.projectfloodlight.openflow.protocol;
-
-import io.netty.buffer.ByteBuf;
-import org.projectfloodlight.openflow.exceptions.OFParseError;
-
-public interface OFMessageReader<T> {
-    T readFrom(ByteBuf bb) throws OFParseError;
-}
